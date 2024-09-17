bfbaca85b08015ff9584d3884666c6c35c247214
Jason Ryan <164967582+orian42@users.noreply.github.com>
2024-09-16 21:03:56 -0700

    added new models to schemas

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,11 @@
 const Profile = require('./profile');
 const Player = require('./player');
+const Team = require('./team');
+const League = require('./league');
 
 module.exports = { 
     Profile, 
-    Player
+    Player,
+    Team,
+    League
 };
