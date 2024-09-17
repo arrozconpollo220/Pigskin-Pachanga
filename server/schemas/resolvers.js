fc26bf6ce474f2d538b8ae3b28fd64dde2d53236
Jason Ryan <164967582+orian42@users.noreply.github.com>
2024-09-17 10:48:25 -0700

    added various mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -20,11 +20,97 @@ const resolvers = {
     },
 
     Mutation: {
+        // Creating new items
         addNewTeam: async (parent, { name }) => {
             return Team.create({name});
         },
         addNewLeague: async (parent, { name, commissioner }) => {
             return League.create({name, commissioner});
+        },
+
+        // Updating existing items
+        addPlayerToTeam: async (parent, { teamId, playerId }) => {
+            return Team.findOneAndUpdate(
+                { _id: teamId},
+                {
+                    $addToSet: { players: playerId }
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            )
+        },
+        removePlayerFromTeam: async (parent, { teamId, playerId }) => {
+            return Team.findOneAndUpdate(
+                { _id: teamId},
+                {
+                    $pull: { players: playerId }
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            )
+        },
+        addTeamToLeague: async (parent, { leagueId, teamId }) => {
+            return League.findOneAndUpdate(
+                { _id: leagueId},
+                {
+                    $addToSet: { teams: teamId }
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            )
+        },
+        removeTeamFromLeague: async (parent, { leagueId, teamId }) => {
+            return League.findOneAndUpdate(
+                { _id: leagueId},
+                {
+                    $pull: { teams: teamId }
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            )
+        },
+        updateTeam: async (parent, { teamId, teamName }) => {
+            return Team.findOneAndUpdate(
+                { _id: teamId},
+                {
+                    $set: { name: teamName }
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            )
+        },
+        updateLeague: async (parent, { leagueId, leagueName, leagueComm }) => {
+            return League.findOneAndUpdate(
+                { _id: leagueId},
+                {
+                    $set: { 
+                        name: leagueName,
+                        commissioner: leagueComm,
+                    }
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            )
+        },
+
+        // Deleting existing items
+        removeTeam: async (parent,  { teamId }) => {
+            return Team.findOneAndDelete({ _id: teamId });
+        },
+        removeLeague: async (parent,  { leagueId }) => {
+            return League.findOneAndDelete({ _id: leagueId });
         },
     }
 };
