a4b3e8c879efb0493ea9c2ccbcbc6cac13e0bb07
Keith Godfrey <keith.godfrey23@gmail.com>
2024-09-17 19:32:27 -0700

    added draft, home, login and match-up to pages folder

---
+++ client/src/pages/Match-up.jsx
@@ -0,0 +1,12 @@
+export default function Match-up() {
+    return (
+      <div>
+        <h2>Match Up</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
+          velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
+          ac dui fermentum, sed luctus urna tincidunt. Etiam 
+        </p>
+      </div>
+    );
+  }
