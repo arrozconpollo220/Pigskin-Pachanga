0effade4de94ca59c8252ca4685a05758fa912be
rickenofficial <70503436+rickenofficial@users.noreply.github.com>
2024-09-20 10:39:43 -0600

    adding styles to the navbar, adding dashboard button.

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,35 +1,48 @@
+import React, { useState, useEffect } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
+import '../styles/NavBar.css';
 
 function NavBar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    
+    const loggedInStatus = localStorage.getItem('isLoggedIn');
+    if (loggedInStatus === 'true') {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const navItems = [
     { to: '/', label: 'Home' },
     { to: '/Match-up', label: 'Match up' },
     { to: '/Draft', label: 'Draft' },
-    { to: '/Login', label: 'Login' },
+    { to: '/Dashboard', label: 'Dashboard'},
   ];
 
   return (
     <Navbar expand="lg" className="bg-body-tertiary" style={{ width: '100vw' }}>
-
       <nav className="nav nav-tabs">
         <Navbar.Brand className="KG">
-          <div class="box">
-
-            <div class="title">
-              <span class="block"></span>
-              <h1><span className="text-primary custom-span">F</span>antasy <span className="text-primary custom-span">F</span>ootball <span className="text-danger custom-span">A</span>pp</h1>
+          <div className="box">
+            <div className="title">
+              <span className="block"></span>
+              <h1>
+                <span className="text-primary custom-span">F</span>antasy 
+                <span className="text-primary custom-span">F</span>ootball 
+                <span className="text-danger custom-span">A</span>pp
+              </h1>
             </div>
-
-            <div class="role">
-              <div class="block"></div>
+            <div className="role">
+              <div className="block"></div>
               <p></p>
             </div>
-
-          </div > 
+          </div>
         </Navbar.Brand>
+
+
         {navItems.map((item) => (
           <Link key={item.to} to={item.to}>
             <Button variant="primary" className="m-3">
@@ -37,8 +50,16 @@ function NavBar() {
             </Button>
           </Link>
         ))}
-      </nav>
 
+        
+        {!isLoggedIn && (
+          <Link to="/Login">
+            <Button variant="primary" className="m-3">
+              Login
+            </Button>
+          </Link>
+        )}
+      </nav>
     </Navbar>
   );
 }
