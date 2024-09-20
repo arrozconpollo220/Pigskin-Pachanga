eb820286ffad9073a34ec9447b1560037bdebb94
Keith Godfrey <keith.godfrey23@gmail.com>
2024-09-19 19:09:45 -0700

    added css to pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from './pages/Footer';
+import Footer from './components/Footer';
+
 import Home from './pages/Home';
 import Login from './pages/Login'; 
+import Draft from './pages/Draft';
+import Headers from './components/Headers';
+import NavBar from './components/NavBar';
 import './App.css';
-import Headers from './pages/Headers'
+import './styles/NavBar.css';
+import './styles/Footer.css';
+import './styles/Headers.css';
 function App() {
   const [email, setEmail] = useState(""); // Asegúrate de definir el estado email
   const [loggedIn, setLoggedIn] = useState(false);
