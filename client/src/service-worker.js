70583c4f2703bda0b6a79937b6b414908e05ff81
Jason Ryan <164967582+orian42@users.noreply.github.com>
2024-09-21 17:29:32 -0700

    added PWA functionality to install as stand-alone app

---
+++ client/src/service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/App.jsx'
+                '/index.css',
+                '/app.css',
+                '/main.jsx',
+                '/icon-192x192.png',
+                '/icon-512x512.png',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
