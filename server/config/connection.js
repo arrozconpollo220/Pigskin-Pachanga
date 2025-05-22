befea04136f9ff6fdaa8d3a66176de0736cd41df
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2025-05-22 09:52:53 -0600

    reversed code in connection.js file

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,6 +4,6 @@ require('dotenv').config();
 const db_username = process.env.DB_USER
 const db_password = process.env.DB_PASSWORD
 
-mongoose.connect(process.env.MONGODB_URI || `mongodb://${db_username}:${db_password}@cluster0.3wiy3.mongodb.net/`);
+mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${db_username}:${db_password}@cluster0.3wiy3.mongodb.net/`);
 
 module.exports = mongoose.connection;
